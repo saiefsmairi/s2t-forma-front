bd3493ed4e221d5d4e502a4a44e54adcf1948f3f
saif smairi <saieftaher@gmail.com>
2020-02-26 15:28:39 +0100

    v1

---
+++ src/app/app.routing.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule, } from '@angular/common';
+import { BrowserModule  } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+
+import { ComponentsComponent } from './components/components.component';
+import { ProfileComponent } from './examples/profile/profile.component';
+import { SignupComponent } from './examples/signup/signup.component';
+import { LandingComponent } from './examples/landing/landing.component';
+import { SignupFormateurComponent } from './signup-formateur/signup-formateur.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+
+const routes: Routes =[
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home',             component: ComponentsComponent },
+    { path: 'user-profile',     component: ProfileComponent },
+    { path: 'signupApprenant',           component: SignupComponent },
+    { path: 'signupFormateur',  component: SignupFormateurComponent },
+    { path: 'signin',  component: SignInComponent },
+
+    { path: 'landing',          component: LandingComponent },
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes,{
+      useHash: true
+    })
+  ],
+  exports: [
+  ],
+})
+export class AppRoutingModule { }
---
+++ src/app/signup-formateur/signup-formateur.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signup-formateur',
+  templateUrl: './signup-formateur.component.html',
+  styleUrls: ['./signup-formateur.component.scss']
+})
+export class SignupFormateurComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
