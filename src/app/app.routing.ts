f9f22d37623a53dd03fdcc0e5ec70db143a43965
Ya39oubi <hamzayaakoubi98@gmail.com>
2020-03-02 15:15:54 +0100

    Cleand Project + angular material installed

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,28 +4,24 @@ import { BrowserModule  } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ComponentsComponent } from './components/components.component';
-import { ProfileComponent } from './examples/profile/profile.component';
-import { SignupComponent } from './examples/signup/signup.component';
-import { LandingComponent } from './examples/landing/landing.component';
-import { SignupFormateurComponent } from './signup-formateur/signup-formateur.component';
-import { SignInComponent } from './sign-in/sign-in.component';
-import {DashboardComponent} from './dashboard/dashboard.component';
-import { ChoixTypeUserComponent } from './choix-type-user/choix-type-user.component';
+import { SignupComponent } from './components/signup/signup.component';
+
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import {DashboardComponent} from './components/dashboard/dashboard.component';
+import { ChoixTypeUserComponent } from './components/choix-type-user/choix-type-user.component';
 
 const routes: Routes =[
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home',             component: ComponentsComponent },
-    { path: 'user-profile',     component: ProfileComponent },
+
     { path: 'choix',   component: ChoixTypeUserComponent },
-    { path: 'signup/Apprenant',   component: SignupComponent },
-    { path: 'signup/Formateur',   component: SignupComponent },
-    { path: 'signup/Societe',  component: SignupComponent },
+    { path: 'signup/:type',  component: SignupComponent },
     { path: 'signin',  component: SignInComponent },
 
     //those not needed for now
-    { path: 'signupFormateur',  component: SignupFormateurComponent },
+
     { path: 'dash',  component: DashboardComponent },
-    { path: 'landing',          component: LandingComponent },
+
 ];
 
 @NgModule({
