b1756441e6bb0a604ba8ad201b908b4fc42a551b
saif smairi <saieftaher@gmail.com>
2020-03-05 21:08:06 +0100

    new ui with home/signup/choix/signin
    and routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { RouterModule } from '@angular/router';
-import { AppRoutingModule } from './app.routing';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './shared/navbar/navbar.component';
-import { FooterComponent } from './shared/footer/footer.component';
-
-import { ComponentsModule } from './components/components.module';
-import { ExamplesModule } from './examples/examples.module';
-import { SignupFormateurComponent } from './signup-formateur/signup-formateur.component';
-import { SignInComponent } from './sign-in/sign-in.component';
-import {HttpClientModule} from '@angular/common/http';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { ChoixTypeUserComponent } from './choix-type-user/choix-type-user.component'; 
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeV2Component } from './home-v2/home-v2.component';
+import { ChoixComponent } from './choix/choix.component';
+import { SigninComponent } from './signin/signin.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SignupComponent } from './signup/signup.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+    HomeV2Component,
+    ChoixComponent,
+    SigninComponent,
     NavbarComponent,
-    FooterComponent,
-    SignupFormateurComponent,
-    SignInComponent,
-    DashboardComponent,
-    ChoixTypeUserComponent
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule.forRoot(),
-    FormsModule,
-    RouterModule,
-    ComponentsModule,
-    ExamplesModule,
     AppRoutingModule,
-    HttpClientModule
+    BrowserAnimationsModule,
+    MatSliderModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
