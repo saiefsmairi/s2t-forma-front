1839963edcedee2d0234162b4af64c9be969e17c
Ya39oubi <hamzayaakoubi98@gmail.com>
2020-03-06 11:20:09 +0100

    cleaned master branch

--- src/app/helpers/auth.interceptor.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Auth.Interceptor } from './auth.interceptor';
-
-describe('Auth.Interceptor', () => {
-  it('should create an instance', () => {
-    expect(new Auth.Interceptor()).toBeTruthy();
-  });
-});
