41a04a24dcf0086c4bb887d18ddc2d74ee26a6ec
Ya39oubi <hamzayaakoubi98@gmail.com>
2020-03-08 22:29:40 +0100

    material form signup formateur

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { CertifDialogComponent } from './certif-dialog/certif-dialog.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -13,6 +14,19 @@ import { MatSliderModule } from '@angular/material/slider';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { SidenavDashboardComponent } from './sidenav-dashboard/sidenav-dashboard.component';
 import { UserProfilComponent } from './user-profil/user-profil.component';
+import { HttpClientModule } from '@angular/common/http';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDialogModule, MAT_DIALOG_DEFAULT_OPTIONS} from '@angular/material/dialog';
+import {MatListModule} from '@angular/material/list';
+
+
+
 
 @NgModule({
   declarations: [
@@ -24,16 +38,36 @@ import { UserProfilComponent } from './user-profil/user-profil.component';
     SignupComponent,
     DashboardComponent,
     SidenavDashboardComponent,
-    UserProfilComponent
+    UserProfilComponent,
+    CertifDialogComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    MatSliderModule,
+    HttpClientModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatListModule
+
+
+
 
   ],
-  providers: [],
+  entryComponents :[
+    CertifDialogComponent
+  ],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,15 +1,126 @@
+import { CertifDialogComponent } from './../certif-dialog/certif-dialog.component';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AuthService } from '../Services/auth.service';
+import { FormControl, Validators, FormGroup, FormBuilder } from '@angular/forms';
+import {
+  MAT_MOMENT_DATE_FORMATS,
+  MomentDateAdapter,
+  MAT_MOMENT_DATE_ADAPTER_OPTIONS
+} from '@angular/material-moment-adapter';
+import {
+  DateAdapter,
+  MAT_DATE_FORMATS,
+  MAT_DATE_LOCALE
+} from '@angular/material/core';
+import { MustMatch } from '../Helpers/MustMatch';
+import { MatDialog } from '@angular/material/dialog';
+
+// tslint:disable-next-line:no-duplicate-imports
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
+  providers: [
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS]
+    },
+    { provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS }
+  ]
 })
 export class SignupComponent implements OnInit {
+  form: FormGroup;
+  isSuccessful = false;
+  isSignUpFailed = false;
+  errorMessage = '';
+  type: string;
+  hide = true;
+  listCertif: Array<any> ;
+  nomCertif: string;
+  dateCertif: string;
+  private sub: any;
+  constructor(
+    private route: ActivatedRoute,
+    private authService: AuthService,
+    private _adapter: DateAdapter<any>,
+    private formBuilder: FormBuilder,
+    public dialog: MatDialog
+  ) {}
 
-  constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.listCertif = new Array();
+    this.sub = this.route.params.subscribe(params => {
+      this.type = params.id;
+    });
+    this._adapter.setLocale('fr');
+    this.form = this.formBuilder.group({
+      email : new FormControl('', [Validators.required, Validators.email]),
+      cin : new FormControl('', [
+        Validators.required,
+        Validators.pattern('[0-9]{8}')
+      ]),
+      nom : new FormControl(''),
+      prenom : new FormControl(''),
+      datenais : new FormControl('', [Validators.required]),
+      password : new FormControl('', [Validators.required]),
+      confirmPass : new FormControl('', [Validators.required]),
+      tel : new FormControl('', [
+        Validators.required,
+        Validators.pattern('[0-9]{8}')
+      ]),
+      rib: new FormControl('', [Validators.required])
+
+    },
+    {validator: MustMatch('password', 'confirmPass')});
   }
+
+  onSubmit() {
+    console.log(this.type);
+    console.log(this.form);
+    this.authService.register(this.f, this.type).subscribe(
+       data => {
+         console.log(data);
+         this.isSuccessful = true;
+         this.isSignUpFailed = false;
+       },
+       err => {
+console.log('breaks here');
+         // this.errorMessage = err.error.message;
+this.isSignUpFailed = true;
+       }
+     );
+   }
+
+
+get f() { return this.form.controls; }
+
+openDialog(): void {
+  const dialogRef = this.dialog.open(CertifDialogComponent, {
+    width: '400px',
+    data: {nom: this.nomCertif, date: this.dateCertif}
+
+  });
+
+  dialogRef.afterClosed().subscribe(result => {
+    console.log('The dialog was closed');
+    if (result != undefined) {
+  this.nomCertif = result.nomCertif.value;
+  this.dateCertif = result.dateCertif.value;
+  this.listCertif.push({
+nom: this.nomCertif,
+date: this.dateCertif
+});
+  console.table(this.listCertif);
+
+}
+
+
+  });
+}
+
 
 }
